236c84dedca9c26f5159920ff60730e4ac2a08f6
Kirill <julich36ru@gmail.com>
2026-08-29 18:01:33 +0300

    29 08 26

--- src/database.py
+++ src/database.py
@@ -4,6 +4,6 @@ DB_PATH.parent.mkdir(parents=True, exist_ok=True)
 
 from pathlib import Path
 
-print(Path.cwd(), "\n")
-print(DB_PATH, "\n")
-print(DB_PATH.resolve())
+# print(Path.cwd(), "\n")
+# print(DB_PATH, "\n")
+# print(DB_PATH.resolve())
